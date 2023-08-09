130ee4f3449356e3af925059f6496143e226740b
Sireesha <agurusireeshaedu@gmail.com>
2023-08-09 12:26:00 +0530

    added

---
+++ Array_School/Check_for_binary.py
@@ -0,0 +1,21 @@
+# Return true if str is binary, else false
+def isBinary(str):
+    #code here
+    for i in str:
+       if(i != '1' and i != '0'):
+           return 0
+    return 1
+
+
+#{ 
+ # Driver Code Starts
+# Your code goes here
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        str = input().strip()
+        if isBinary(str):
+            print (1)
+        else:
+            print (0)
+# } Driver Code Ends
